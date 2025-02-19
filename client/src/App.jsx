f2184806dac1e1ed74693b3c6fbbc19563235754
Naini-magic <nraghuwanshi00401@gmail.com>
2025-02-19 23:53:13 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='bg-blue-200'>hello</div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+const app = express();
+dotenv.config();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+const PORT  = process.env.PORT || 5001;
+app.listen(PORT , () => {
+    console.log(`Server running on port ${PORT}`);
+})
