c48e7885559ca037bac4f12c419eb9631c20fe16
Naini-magic <nraghuwanshi00401@gmail.com>
2025-03-15 22:36:55 +0530

    15-march2

--- server/src/service/NotificationService.js
+++ server/src/service/NotificationService.js
@@ -1,20 +1,20 @@
-const admin = require("../util/firebase_backend");
+// const admin = require("../util/firebase_backend");
 
-class NotificationService {
-    static async sendNotification(deviceToken , title , body) {
-        const message = {
-            notification : {
-                title , body
-            },
-            token : deviceToken
-        };
-        try{
-            const response = await admin.messaging().send(message);
-            return response;
-        }catch(error){
-            throw error;
-        }
-    }
-};
+// class NotificationService {
+//     static async sendNotification(deviceToken , title , body) {
+//         const message = {
+//             notification : {
+//                 title , body
+//             },
+//             token : deviceToken
+//         };
+//         try{
+//             const response = await admin.messaging().send(message);
+//             return response;
+//         }catch(error){
+//             throw error;
+//         }
+//     }
+// };
 
-module.exports = NotificationService;
+// module.exports = NotificationService;
