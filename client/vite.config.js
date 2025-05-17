1ce004832b8c0755f7263dd0b84acbd308633e53
Naini-magic <nraghuwanshi00401@gmail.com>
2025-05-17 23:33:33 +0530

    fix: add CSS handling for toastify in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync("./localhost.key"),
-  //     cert: fs.readFileSync("./localhost.crt"),
-  //   },
-  // },
+  build: {
+    // This ensures toastify CSS gets included in production
+    assetsInclude: ['**/*.css'],
+    // Optional but recommended for performance
+    cssCodeSplit: true
+  }
 });
